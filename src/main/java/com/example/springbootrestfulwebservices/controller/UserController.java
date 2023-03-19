ffd7ea8d8a9c6d2d3a5ee02d5aa734e715507787
stark <anishgupta0066@gmail.com>
2023-03-19 11:59:56 +0530

    added rest apis for get, update

--- src/main/java/com/example/springbootrestfulwebservices/controller/UserController.java
+++ src/main/java/com/example/springbootrestfulwebservices/controller/UserController.java
@@ -6,10 +6,9 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @AllArgsConstructor
@@ -24,4 +23,25 @@ public class UserController {
         return new ResponseEntity<>(savedUser, HttpStatus.CREATED);
     }
 
+    //build get user by id REST api
+    @GetMapping("{id}")
+    public ResponseEntity<User> getUserById(@PathVariable("id") Long userId){
+        User user = userService.getUserById(userId);
+        return new ResponseEntity<>(user, HttpStatus.OK);
+    }
+
+    // build get all users REST api
+    @GetMapping()
+    public ResponseEntity<List<User>> getAllUsers(){
+        List<User> users = userService.getAllUsers();
+        return new ResponseEntity<>(users, HttpStatus.OK);
+    }
+
+    // build update User REST api
+    @PutMapping("{id}")
+    public ResponseEntity<User> updateUser(@PathVariable("id")Long userId,@RequestBody User user){
+        user.setId(userId);
+        User updatedUser = userService.updateUser(user);
+        return new ResponseEntity<>(updatedUser, HttpStatus.OK);
+    }
 }
--- src/main/java/com/example/springbootrestfulwebservices/service/UserService.java
+++ src/main/java/com/example/springbootrestfulwebservices/service/UserService.java
@@ -2,6 +2,14 @@ package com.example.springbootrestfulwebservices.service;
 
 import com.example.springbootrestfulwebservices.entity.User;
 
+import java.util.List;
+
 public interface UserService {
     User createUser(User user);
+
+    User getUserById(Long userId);
+
+    List<User> getAllUsers();
+
+    User updateUser(User user);
 }
