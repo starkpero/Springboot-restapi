038e47821272eafebf7ee808072de48a9cbb0e76
stark <anishgupta0066@gmail.com>
2023-04-08 23:46:00 +0530

    added description for swagger UI

--- src/main/java/com/example/springbootrestfulwebservices/SpringbootRestfulWebservicesApplication.java
+++ src/main/java/com/example/springbootrestfulwebservices/SpringbootRestfulWebservicesApplication.java
@@ -1,11 +1,30 @@
 package com.example.springbootrestfulwebservices;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.info.License;
 import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@OpenAPIDefinition(
+		info = @Info(
+				title = "Spring boot REST API doc",
+				description = "documentation",
+				version = "v1.0",
+				contact = @Contact(
+						name = "Stark",
+						email = "Starkpero007@gmail.com",
+						url = "https://portfolio-lovat-omega.vercel.app/"
+				),
+				license = @License(
+						name = "Apache 2.0"
+				)
+		)
+)
 public class SpringbootRestfulWebservicesApplication {
 
 	@Bean
